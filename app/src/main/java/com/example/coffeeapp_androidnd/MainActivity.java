28bc8a708fe8b680046ecac551e9df82d4cc82d3
eslam <esla.ahme@hotmail.com>
2019-10-26 22:18:27 +0200

    arranged

--- app/src/main/java/com/example/coffeeapp_androidnd/MainActivity.java
+++ app/src/main/java/com/example/coffeeapp_androidnd/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.coffeeapp_androidnd;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import java.text.NumberFormat;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
     private int quantity = 2;
@@ -19,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void submitOrder(View view) {
 
-        String priceMessage = "price " + (quantity*5) +"\n"+"Thank you";
+        String priceMessage = "price " + (quantity * 5) + "\n" + "Thank you";
         displayMessage(priceMessage);
     }
 
@@ -28,13 +27,13 @@ public class MainActivity extends AppCompatActivity {
      */
     private void display(int number) {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText(number);
     }
 
-    private void displayPrice(int number) {
+   /* private void displayPrice(int number) {
         TextView priceTextView = findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+    }*/
 
     public void increament(View view) {
         quantity += 1;
@@ -51,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = findViewById(R.id.price_text_view);
         priceTextView.setText(message);
     }
 }
